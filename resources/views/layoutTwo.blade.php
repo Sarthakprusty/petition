6f6b6744196aa6122f295c37ed4ff248302d5afb
Sayantan Saha <sayantan.somu@gmail.com>
2023-08-28 15:47:03 +0530

    Updated assets url 2

--- resources/views/layoutTwo.blade.php
+++ resources/views/layoutTwo.blade.php
@@ -10,13 +10,13 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link href="/css/app.css" rel="stylesheet" />
+    <link href="{{url(asset('css/app.css'))}}" rel="stylesheet" />
 
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 
     <script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-    <script src="/js/tinymce_6.4.2/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="{{url(asset('js/tinymce_6.4.2/tinymce.min.js'))}}" referrerpolicy="origin"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
