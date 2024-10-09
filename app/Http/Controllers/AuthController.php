885f6963ba7bda11533d57ea649e4b1af7493eef
Sarthak <sarthak@rb.nic.in>
2024-10-09 15:36:45 +0530

    9.10.24

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
 
         if (Auth::attempt($credentials)) {
             if (Auth::user()->active == 1)
-                return redirect(route('applications.index'));
+                return redirect(route('applications.dashboard'));
             else {
                 Auth::logout();
                 return Redirect::to(route('login'));
